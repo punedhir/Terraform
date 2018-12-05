df928ed617d21b7a616a9803bc0f503e2df05ab9
punedhir <42438754+punedhir@users.noreply.github.com>
2018-12-05 19:56:15 +0530

    Update puppetagent.sh

--- azure/puppetagent.sh
+++ azure/puppetagent.sh
@@ -1,4 +1,4 @@
 wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 dpkg -i puppetlabs-release-pc1-xenial.deb
-apt-get update
-apt-get install puppet-agent
+apt-get update -y
+apt-get install puppet-agent -y
