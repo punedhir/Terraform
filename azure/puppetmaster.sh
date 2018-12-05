bbaa6aec74653e91f18b59c4eafa28de32603ae2
punedhir <42438754+punedhir@users.noreply.github.com>
2018-12-05 19:37:02 +0530

    Update puppetmaster.sh

--- azure/puppetmaster.sh
+++ azure/puppetmaster.sh
@@ -1,5 +1,6 @@
 curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 dpkg -i puppetlabs-release-pc1-xenial.deb
-apt-get --assume-yes update
-apt-get --assume-yes install puppetserverufw allow 8140
+apt-get update
+apt-get install puppetserver
+ufw allow 8140
 systemctl start puppetserver
