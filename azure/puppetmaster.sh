26ea023ca80f5b6e685a4f27ed42560afa6b9e00
punedhir <42438754+punedhir@users.noreply.github.com>
2018-12-05 19:53:58 +0530

    Update puppetmaster.sh

--- azure/puppetmaster.sh
+++ azure/puppetmaster.sh
@@ -1,5 +1,5 @@
 curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 dpkg -i puppetlabs-release-pc1-xenial.deb
-apt-get update
-apt-get install puppetserver
+apt-get update -y
+apt-get install puppetserver -y
 
